33601a3d01023a01da073dd2ace3c37d99b82775
Paul Nixon <pauljnixon@gmail.com>
2014-01-27 13:11:40 -0500

    all sides of the cube work but nothing else yet

--- homework/1/Assignment1-Shape/Cube.h
+++ homework/1/Assignment1-Shape/Cube.h
@@ -9,12 +9,150 @@ public:
 	~Cube() {};
 
 	void draw() {
-
+      glPushMatrix();
+      glBegin(GL_TRIANGLES);
+      int i,j,k;
+      
+      for(i=0; i<m_segmentsX; i++){
+          for(j=0; j<m_segmentsY; j++){
+              //front face (+z)
+              glNormal3f(0.0f, 0.0f, 1.0f);
+              glVertex3f(-0.5f+i*(1.0f/m_segmentsX), -0.5f+j*(1.0f/m_segmentsY), 0.5f);
+              glVertex3f(-0.5f+i*(1.0f/m_segmentsX), -0.5f+(j+1)*(1.0f/m_segmentsY), 0.5f);
+              glVertex3f(-0.5f+(i+1)*(1.0f/m_segmentsX), -0.5f+j*(1.0f/m_segmentsY), 0.5f);
+              
+              glVertex3f(-0.5f+i*(1.0f/m_segmentsX), -0.5f+(j+1)*(1.0f/m_segmentsY), 0.5f);
+              glVertex3f(-0.5f+(i+1)*(1.0f/m_segmentsX), -0.5f+(j+1)*(1.0f/m_segmentsY), 0.5f);
+              glVertex3f(-0.5f+(i+1)*(1.0f/m_segmentsX), -0.5f+j*(1.0f/m_segmentsY), 0.5f);
+              
+              //back face (-z)
+              glNormal3f(0.0f, 0.0f, -1.0f);
+              glVertex3f(-0.5f+(i+1)*(1.0f/m_segmentsX), -0.5f+j*(1.0f/m_segmentsY), -0.5f);
+              glVertex3f(-0.5f+i*(1.0f/m_segmentsX), -0.5f+(j+1)*(1.0f/m_segmentsY), -0.5f);
+              glVertex3f(-0.5f+i*(1.0f/m_segmentsX), -0.5f+j*(1.0f/m_segmentsY), -0.5f);
+              
+              glVertex3f(-0.5f+i*(1.0f/m_segmentsX), -0.5f+(j+1)*(1.0f/m_segmentsY), -0.5f);
+              glVertex3f(-0.5f+(i+1)*(1.0f/m_segmentsX), -0.5f+j*(1.0f/m_segmentsY), -0.5f);
+              glVertex3f(-0.5f+(i+1)*(1.0f/m_segmentsX), -0.5f+(j+1)*(1.0f/m_segmentsY), -0.5f);
+              
+              //right (+x) face
+              glNormal3f(1.0f, 0.0f, 0.0f);
+              glVertex3f(0.5f, -0.5f+j*(1.0f/m_segmentsY), -0.5f+i*(1.0f/m_segmentsX));
+              glVertex3f(0.5f, -0.5f+(j+1)*(1.0f/m_segmentsY), -0.5f+i*(1.0f/m_segmentsX));
+              glVertex3f(0.5f, -0.5f+j*(1.0f/m_segmentsY), -0.5f+(i+1)*(1.0f/m_segmentsX));
+              
+              glVertex3f(0.5f, -0.5f+(j+1)*(1.0f/m_segmentsY), -0.5f+(i+1)*(1.0f/m_segmentsX));
+              glVertex3f(0.5f, -0.5f+(j+1)*(1.0f/m_segmentsY), -0.5f+i*(1.0f/m_segmentsX));
+              glVertex3f(0.5f, -0.5f+j*(1.0f/m_segmentsY), -0.5f+(i+1)*(1.0f/m_segmentsX));
+              
+              //left (-x) face
+              glNormal3f(-1.0f, 0.0f, 0.0f);
+              glVertex3f(-0.5f, -0.5f+j*(1.0f/m_segmentsY), -0.5f+i*(1.0f/m_segmentsX));
+              glVertex3f(-0.5f, -0.5f+(j+1)*(1.0f/m_segmentsY), -0.5f+i*(1.0f/m_segmentsX));
+              glVertex3f(-0.5f, -0.5f+j*(1.0f/m_segmentsY), -0.5f+(i+1)*(1.0f/m_segmentsX));
+              
+              glVertex3f(-0.5f, -0.5f+(j+1)*(1.0f/m_segmentsY), -0.5f+(i+1)*(1.0f/m_segmentsX));
+              glVertex3f(-0.5f, -0.5f+(j+1)*(1.0f/m_segmentsY), -0.5f+i*(1.0f/m_segmentsX));
+              glVertex3f(-0.5f, -0.5f+j*(1.0f/m_segmentsY), -0.5f+(i+1)*(1.0f/m_segmentsX));
+              
+              //top (+y) face
+              glNormal3f(0.0f, 1.0f, 0.0f);
+              glVertex3f(-0.5f+j*(1.0f/m_segmentsY), 0.5f, -0.5f+i*(1.0f/m_segmentsX));
+              glVertex3f(-0.5f+(j+1)*(1.0f/m_segmentsY), 0.5f, -0.5f+i*(1.0f/m_segmentsX));
+              glVertex3f(-0.5f+j*(1.0f/m_segmentsY), 0.5f, -0.5f+(i+1)*(1.0f/m_segmentsX));
+              
+              glVertex3f(-0.5f+(j+1)*(1.0f/m_segmentsY), 0.5f, -0.5f+(i+1)*(1.0f/m_segmentsX));
+              glVertex3f(-0.5f+(j+1)*(1.0f/m_segmentsY), 0.5f, -0.5f+i*(1.0f/m_segmentsX));
+              glVertex3f(-0.5f+j*(1.0f/m_segmentsY), 0.5f, -0.5f+(i+1)*(1.0f/m_segmentsX));
+              
+              //bottom (-y) face
+              glNormal3f(0.0f, -1.0f, 0.0f);
+              glVertex3f(-0.5f+j*(1.0f/m_segmentsY), -0.5f, -0.5f+i*(1.0f/m_segmentsX));
+              glVertex3f(-0.5f+(j+1)*(1.0f/m_segmentsY), -0.5f, -0.5f+i*(1.0f/m_segmentsX));
+              glVertex3f(-0.5f+j*(1.0f/m_segmentsY), -0.5f, -0.5f+(i+1)*(1.0f/m_segmentsX));
+              
+              glVertex3f(-0.5f+(j+1)*(1.0f/m_segmentsY), -0.5f, -0.5f+(i+1)*(1.0f/m_segmentsX));
+              glVertex3f(-0.5f+(j+1)*(1.0f/m_segmentsY), -0.5f, -0.5f+i*(1.0f/m_segmentsX));
+              glVertex3f(-0.5f+j*(1.0f/m_segmentsY), -0.5f, -0.5f+(i+1)*(1.0f/m_segmentsX));
+          }
+      }
+      
+      glEnd();
+      glPopMatrix();
 	};
 
 
 	void drawNormal() {
-
+      glPushMatrix();
+      glBegin(GL_TRIANGLES);
+      int i,j,k;
+      
+      for(i=0; i<m_segmentsX; i++){
+          for(j=0; j<m_segmentsY; j++){
+              //front face (+z)
+              glNormal3f(0.0f, 0.0f, 1.0f);
+              glVertex3f(-0.5f+i*(1.0f/m_segmentsX), -0.5f+j*(1.0f/m_segmentsY), 0.5f);
+              glVertex3f(-0.5f+i*(1.0f/m_segmentsX), -0.5f+(j+1)*(1.0f/m_segmentsY), 0.5f);
+              glVertex3f(-0.5f+(i+1)*(1.0f/m_segmentsX), -0.5f+j*(1.0f/m_segmentsY), 0.5f);
+              
+              glVertex3f(-0.5f+i*(1.0f/m_segmentsX), -0.5f+(j+1)*(1.0f/m_segmentsY), 0.5f);
+              glVertex3f(-0.5f+(i+1)*(1.0f/m_segmentsX), -0.5f+(j+1)*(1.0f/m_segmentsY), 0.5f);
+              glVertex3f(-0.5f+(i+1)*(1.0f/m_segmentsX), -0.5f+j*(1.0f/m_segmentsY), 0.5f);
+              
+              //back face (-z)
+              glNormal3f(0.0f, 0.0f, -1.0f);
+              glVertex3f(-0.5f+(i+1)*(1.0f/m_segmentsX), -0.5f+j*(1.0f/m_segmentsY), -0.5f);
+              glVertex3f(-0.5f+i*(1.0f/m_segmentsX), -0.5f+(j+1)*(1.0f/m_segmentsY), -0.5f);
+              glVertex3f(-0.5f+i*(1.0f/m_segmentsX), -0.5f+j*(1.0f/m_segmentsY), -0.5f);
+              
+              glVertex3f(-0.5f+i*(1.0f/m_segmentsX), -0.5f+(j+1)*(1.0f/m_segmentsY), -0.5f);
+              glVertex3f(-0.5f+(i+1)*(1.0f/m_segmentsX), -0.5f+j*(1.0f/m_segmentsY), -0.5f);
+              glVertex3f(-0.5f+(i+1)*(1.0f/m_segmentsX), -0.5f+(j+1)*(1.0f/m_segmentsY), -0.5f);
+              
+              //right (+x) face
+              glNormal3f(1.0f, 0.0f, 0.0f);
+              glVertex3f(0.5f, -0.5f+j*(1.0f/m_segmentsY), -0.5f+i*(1.0f/m_segmentsX));
+              glVertex3f(0.5f, -0.5f+(j+1)*(1.0f/m_segmentsY), -0.5f+i*(1.0f/m_segmentsX));
+              glVertex3f(0.5f, -0.5f+j*(1.0f/m_segmentsY), -0.5f+(i+1)*(1.0f/m_segmentsX));
+              
+              glVertex3f(0.5f, -0.5f+(j+1)*(1.0f/m_segmentsY), -0.5f+(i+1)*(1.0f/m_segmentsX));
+              glVertex3f(0.5f, -0.5f+(j+1)*(1.0f/m_segmentsY), -0.5f+i*(1.0f/m_segmentsX));
+              glVertex3f(0.5f, -0.5f+j*(1.0f/m_segmentsY), -0.5f+(i+1)*(1.0f/m_segmentsX));
+              
+              //left (-x) face
+              glNormal3f(-1.0f, 0.0f, 0.0f);
+              glVertex3f(-0.5f, -0.5f+j*(1.0f/m_segmentsY), -0.5f+i*(1.0f/m_segmentsX));
+              glVertex3f(-0.5f, -0.5f+(j+1)*(1.0f/m_segmentsY), -0.5f+i*(1.0f/m_segmentsX));
+              glVertex3f(-0.5f, -0.5f+j*(1.0f/m_segmentsY), -0.5f+(i+1)*(1.0f/m_segmentsX));
+              
+              glVertex3f(-0.5f, -0.5f+(j+1)*(1.0f/m_segmentsY), -0.5f+(i+1)*(1.0f/m_segmentsX));
+              glVertex3f(-0.5f, -0.5f+(j+1)*(1.0f/m_segmentsY), -0.5f+i*(1.0f/m_segmentsX));
+              glVertex3f(-0.5f, -0.5f+j*(1.0f/m_segmentsY), -0.5f+(i+1)*(1.0f/m_segmentsX));
+              
+              //top (+y) face
+              glNormal3f(0.0f, 1.0f, 0.0f);
+              glVertex3f(-0.5f+j*(1.0f/m_segmentsY), 0.5f, -0.5f+i*(1.0f/m_segmentsX));
+              glVertex3f(-0.5f+(j+1)*(1.0f/m_segmentsY), 0.5f, -0.5f+i*(1.0f/m_segmentsX));
+              glVertex3f(-0.5f+j*(1.0f/m_segmentsY), 0.5f, -0.5f+(i+1)*(1.0f/m_segmentsX));
+              
+              glVertex3f(-0.5f+(j+1)*(1.0f/m_segmentsY), 0.5f, -0.5f+(i+1)*(1.0f/m_segmentsX));
+              glVertex3f(-0.5f+(j+1)*(1.0f/m_segmentsY), 0.5f, -0.5f+i*(1.0f/m_segmentsX));
+              glVertex3f(-0.5f+j*(1.0f/m_segmentsY), 0.5f, -0.5f+(i+1)*(1.0f/m_segmentsX));
+              
+              //bottom (-y) face
+              glNormal3f(0.0f, -1.0f, 0.0f);
+              glVertex3f(-0.5f+j*(1.0f/m_segmentsY), -0.5f, -0.5f+i*(1.0f/m_segmentsX));
+              glVertex3f(-0.5f+(j+1)*(1.0f/m_segmentsY), -0.5f, -0.5f+i*(1.0f/m_segmentsX));
+              glVertex3f(-0.5f+j*(1.0f/m_segmentsY), -0.5f, -0.5f+(i+1)*(1.0f/m_segmentsX));
+              
+              glVertex3f(-0.5f+(j+1)*(1.0f/m_segmentsY), -0.5f, -0.5f+(i+1)*(1.0f/m_segmentsX));
+              glVertex3f(-0.5f+(j+1)*(1.0f/m_segmentsY), -0.5f, -0.5f+i*(1.0f/m_segmentsX));
+              glVertex3f(-0.5f+j*(1.0f/m_segmentsY), -0.5f, -0.5f+(i+1)*(1.0f/m_segmentsX));
+          }
+      }
+      
+      glEnd();
+      glPopMatrix();
 	};
 
 private:
--- homework/1/Assignment1-Shape/Cylinder.h
+++ homework/1/Assignment1-Shape/Cylinder.h
@@ -14,7 +14,5 @@ public:
 	void drawNormal() {
 	};
 
-
-private:
-
+};
 #endif
