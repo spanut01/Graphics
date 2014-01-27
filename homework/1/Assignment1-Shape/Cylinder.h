b69ca5c0ee3ddba0a007b98e90809c3c5b0a52e8
Paul Nixon <pauljnixon@gmail.com>
2014-01-27 13:45:50 -0500

    having cylinder problems

--- homework/1/Assignment1-Shape/Cylinder.h
+++ homework/1/Assignment1-Shape/Cylinder.h
@@ -9,6 +9,23 @@ public:
 	~Cylinder() {};
 
 	void draw() {
+      glPushMatrix();
+      glBegin(GL_TRIANGLES);
+      int i,j,k;
+      
+      for(i=0; i<m_segmentsX; i++){
+          //top
+          glNormal3f(0.0f, 1.0f, 0.0f);
+          glNormal3f(0.0f, 0.5f, 0.0f);
+          glVertex3f(0.5f*cosf(i*PI*(2.0f/m_segmentsX)), 0.5f, 0.5f*sinf(j*PI*(2.0f/m_segmentsY)));
+          glVertex3f(0.5f*cosf((i+1)*PI*(2.0f/m_segmentsX)), 0.5f, 0.5f*sinf((j+1)*PI*(2.0f/m_segmentsY)));
+          
+          for(j=0; j<m_segmentsY; j++){
+          }
+      }
+      
+      glEnd();
+      glPopMatrix();
 	};
 
 	void drawNormal() {
