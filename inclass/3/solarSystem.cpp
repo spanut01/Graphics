db12dbf3144766a9f1805216c5e566d8defcb4a7
Paul Nixon <pauljnixon@gmail.com>
2014-02-13 13:27:34 -0500

    tiny bit more solar system

--- inclass/3/solarSystem.cpp
+++ inclass/3/solarSystem.cpp
@@ -38,23 +38,30 @@ void solarSystem::render(){
 
         glPushMatrix();
             glRotatef(rotations[0],0,1,0);
-            glTranslatef(1.5,0.0,1.5);
+            glTranslatef(1.3,0.0,1.5);
             glColor3f(0.7,0.7,0.7);
-            glutSolidSphere(0.15f,10,10);
+            glutSolidSphere(0.05f,10,10);
         glPopMatrix();
 
         glPushMatrix();
             glRotatef(rotations[1],0,1,0);
             glTranslatef(2.1,0.0,2.1);
             glColor3f(0.6,0.4,0.0);
-            glutSolidSphere(0.2f,10,10);
+            glutSolidSphere(0.1f,10,10);
         glPopMatrix();
 
         glPushMatrix();
             glRotatef(rotations[2],0,1,0);
-            glTranslatef(3.0,0.0,3.0);
+            glTranslatef(2.7,0.0,3.0);
             glColor3f(0.0,0.6,0.6);
-            glutSolidSphere(0.25f,10,10);
+            glutSolidSphere(0.15f,10,10);
+        glPopMatrix();
+
+        glPushMatrix();
+            glRotatef(rotations[2],0,1,0);
+            glTranslatef(3.9,0.0,3.0);
+            glColor3f(0.5,0.0,0.0);
+            glutSolidSphere(0.15f,10,10);
         glPopMatrix();
 
         for(i=0; i<10; i++)rotations[i]+=((float)(i+1))/3.0;
